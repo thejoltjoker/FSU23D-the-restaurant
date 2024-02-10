e44a824e7fcf934ab4e8762f3a202f7a9cf52b8c
Johannes Andersson <hello@thejoltjoker.com>
2024-02-10 14:45:46 +0000

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+import { defaultTheme } from "tailwindcss/defaultTheme";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Itim", ...defaultTheme.fontFamily.sans],
+        heading: ["Calistoga", "Itim", ...defaultTheme.fontFamily.serif],
+      },
+      spacing: {
+        sm: "1rem",
+        md: "2rem",
+        lg: "3rem",
+        xl: "4rem",
+      },
       colors: {
-          paleyellow: '#F9C96C',
-          vividorange: '#D7472A',
-          orange: '#DB5C25',
-          darkgreen: '#689B41',
-          darkred: '#790117',
-          almostwhite: '#F9F4EC',
-          paleyellowvariant: '#EDB445',
-          vividorangevariant: '#AC3820',
-          orangevariant: '#AF491D',
-          darkgreenvariant: '#507732',
-          darkredvariant: '#47010D',
-          almostwhitevariant: '#EDDDC5',
-        },
+        "pale-yellow": "#F9C96C",
+        "vivid-orange": "#D7472A",
+        orange: "#DB5C25",
+        "dark-green": "#689B41",
+        "dark-red": "#790117",
+        "almost-white": "#F9F4EC",
+        "pale-yellow-variant": "#EDB445",
+        "vivid-orange-variant": "#AC3820",
+        "orange-variant": "#AF491D",
+        "dark-green-variant": "#507732",
+        "dark-red-variant": "#47010D",
+        "almost-white-variant": "#EDDDC5",
+      },
     },
   },
   plugins: [],
