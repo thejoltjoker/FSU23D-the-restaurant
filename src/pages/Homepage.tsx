d2e7a1e2f9f8934d8e739522f1775c00f7408bd8
Johannes Andersson <hello@thejoltjoker.com>
2024-02-12 11:56:02 +0000

    Add WavySection with example

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,17 +1,19 @@
-import SquigglySection from "../components/SquigglySection";
+import WavySection from "../components/WavySection";
 
 const Homepage = () => {
   return (
     <div>
-      <SquigglySection>
-        <h1>My Squiggly Section</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus at
-          incidunt impedit inventore! Aut expedita fugiat, laborum esse
-          perferendis eaque quis quae deserunt facilis? Quas debitis inventore
-          praesentium consequuntur? Dolor?
-        </p>
-      </SquigglySection>
+      <WavySection bgColor="dark-green" top={true} bottom={true}>
+        <div className="mx-auto max-w-screen-lg py-xl text-almost-white">
+          <h1>My Squiggly Section</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus at
+            incidunt impedit inventore! Aut expedita fugiat, laborum esse
+            perferendis eaque quis quae deserunt facilis? Quas debitis inventore
+            praesentium consequuntur? Dolor?
+          </p>
+        </div>
+      </WavySection>
     </div>
   );
 };
