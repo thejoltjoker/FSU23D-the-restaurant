994e9f1da0a287501c26cfad74817d7cf42296a5
Johannes Andersson <hello@thejoltjoker.com>
2024-02-12 08:40:35 +0000

    Add basename to router

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,40 +5,42 @@ import Errorpage from "./pages/Errorpage";
 import Homepage from "./pages/Homepage";
 import Layout from "./pages/Layout";
 import Menupage from "./pages/Menupage";
-import {createBrowserRouter} from "react-router-dom"
+import { createBrowserRouter } from "react-router-dom";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <Errorpage />,
-    children: [
-
-      {
+export const router = createBrowserRouter(
+  [
+    {
       path: "/",
-      element: <Homepage />,
-      errorElement: <Errorpage />
+      element: <Layout />,
+      errorElement: <Errorpage />,
+      children: [
+        {
+          path: "/",
+          element: <Homepage />,
+          errorElement: <Errorpage />,
+        },
+        {
+          path: "/contact",
+          element: <Contactpage />,
+          errorElement: <Errorpage />,
+        },
+        {
+          path: "/booking",
+          element: <Bookingpage />,
+          errorElement: <Errorpage />,
+        },
+        {
+          path: "/menu",
+          element: <Menupage />,
+          errorElement: <Errorpage />,
+        },
+        {
+          path: "/admin",
+          element: <Adminpage />,
+          errorElement: <Errorpage />,
+        },
+      ],
     },
-    {
-      path: "/contact",
-      element: <Contactpage />,
-      errorElement: <Errorpage />
-    },
-    {
-      path: "/booking",
-      element: <Bookingpage />,
-      errorElement: <Errorpage />
-    },
-    {
-      path: "/menu",
-      element: <Menupage />,
-      errorElement: <Errorpage />
-    },
-    {
-      path: "/admin",
-      element: <Adminpage />,
-      errorElement: <Errorpage />
-    }
-    ],
-  }
-]);
+  ],
+  { basename: "/the-restaurant-grupp1/" },
+);
