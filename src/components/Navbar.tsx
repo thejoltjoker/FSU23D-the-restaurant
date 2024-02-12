00094771ba08a52a1305fb340b788689ed6e0b72
Kaisa Evikoski <kaisa.evikoski@gmail.com>
2024-02-12 14:46:06 +0100

    Make navbar layout ready

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,32 @@
-import { getImageUrl } from "../helpers/strings"
-
+import { getImageUrl } from "../helpers/strings";
 
 const Navbar = () => {
   return (
-    <div className="bg-pale-yellow">
-      <div>
-        <img src={getImageUrl("BlackLogo.svg")} alt="Black Vaca Caliente logo"/>
+    <div className=" flex w-screen justify-between bg-pale-yellow">
+      <div className="ml-4 h-[120px] w-[120px] pt-4">
+        <img
+          src={getImageUrl("logo_black.png")}
+          alt="Black Vaca Caliente logo"
+        />
       </div>
-      
+      <div className="mr-5 pt-5">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="h-8 w-8"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
