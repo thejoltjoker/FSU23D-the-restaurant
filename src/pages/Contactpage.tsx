f91b9c24bd3284f56fa26d487711b5d83215cf00
Kaisa Evikoski <kaisa.evikoski@gmail.com>
2024-02-15 11:38:48 +0100

    Make page more responsiv

--- src/pages/Contactpage.tsx
+++ src/pages/Contactpage.tsx
@@ -36,47 +36,51 @@ const Contactpage = () => {
       <div id="map" className="h-[500px] w-full"></div>
       <div className="-m-wave">
         <WavySection bgColor="dark-green" top={true} bottom={false}>
-          <div className="flex flex-grow flex-col items-center bg-dark-green">
-            <h4 className="mt-8 text-xl text-almost-white">Contact us</h4>
-            <div className="mt-4 flex">
-              <FaMapPin className="mr-1 text-almost-white" />
-              <p className="text-sm text-almost-white">Mexikanska gatan 1</p>
+          <div className="flex flex-wrap">
+            <div className="flex w-full flex-col items-center bg-dark-green md:w-1/2 lg:w-1/6">
+              <h4 className="mt-8 text-xl text-almost-white">Contact us</h4>
+              <div className="mt-4 flex">
+                <FaMapPin className="mr-1 text-almost-white" />
+                <p className="text-sm text-almost-white">Mexikanska gatan 1</p>
+              </div>
+              <p className="text-sm text-almost-white">723 52 Stockholm</p>
+              <div className="mt-3 flex">
+                <FaPhoneAlt className="mr-1 text-almost-white" />
+                <p className="text-sm text-almost-white">+46 123 46 78 90</p>
+              </div>
+              <div className="mb-5 mt-3 flex flex-row">
+                <FaEnvelope className="mr-1 text-almost-white" />
+                <p className="text-sm text-almost-white">
+                  info.vacacaliente.se
+                </p>
+              </div>
             </div>
-            <p className="text-sm text-almost-white">723 52 Stockholm</p>
-            <div className="mt-3 flex">
-              <FaPhoneAlt className="mr-1 text-almost-white" />
-              <p className="text-sm text-almost-white">+46 123 46 78 90</p>
+            <div className="flex w-full flex-grow flex-col items-center bg-dark-green md:w-1/2 lg:w-1/6">
+              <h4 className="mt-6 text-xl text-pale-yellow-variant">
+                Working hours
+              </h4>
+              <p className="mt-1 text-sm text-pale-yellow-variant">
+                Mon 18:00 - 00:00
+              </p>
+              <p className="mt-1 text-sm text-pale-yellow-variant">
+                Tue 18:00 - 00:00
+              </p>
+              <p className="mt-1 text-sm text-pale-yellow-variant">
+                Wed 18:00 - 00:00
+              </p>
+              <p className="mt-1 text-sm text-pale-yellow-variant">
+                Thu 18:00 - 00:00
+              </p>
+              <p className="mt-1 text-sm text-pale-yellow-variant">
+                Fri 18:00 - 00:00
+              </p>
+              <p className="mt-1 text-sm text-pale-yellow-variant">
+                Sat 18:00 - 00:00
+              </p>
+              <p className="mb-3 mt-1 text-sm text-pale-yellow-variant">
+                Sun 18:00 - 00:00
+              </p>
             </div>
-            <div className="mb-5 mt-3 flex flex-row">
-              <FaEnvelope className="mr-1 text-almost-white" />
-              <p className="text-sm text-almost-white">info.vacacaliente.se</p>
-            </div>
-          </div>
-          <div className="flex flex-grow flex-col items-center bg-dark-green">
-            <h4 className="mt-6 text-xl text-pale-yellow-variant">
-              Working hours
-            </h4>
-            <p className="mt-1 text-sm text-pale-yellow-variant">
-              Mon 18:00 - 00:00
-            </p>
-            <p className="mt-1 text-sm text-pale-yellow-variant">
-              Tue 18:00 - 00:00
-            </p>
-            <p className="mt-1 text-sm text-pale-yellow-variant">
-              Wed 18:00 - 00:00
-            </p>
-            <p className="mt-1 text-sm text-pale-yellow-variant">
-              Thu 18:00 - 00:00
-            </p>
-            <p className="mt-1 text-sm text-pale-yellow-variant">
-              Fri 18:00 - 00:00
-            </p>
-            <p className="mt-1 text-sm text-pale-yellow-variant">
-              Sat 18:00 - 00:00
-            </p>
-            <p className="mb-3 mt-1 text-sm text-pale-yellow-variant">
-              Sun 18:00 - 00:00
-            </p>
           </div>
         </WavySection>
       </div>
