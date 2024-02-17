8fdc6cd876468c7d252fad7f617c9a1f7fc63100
Johannes Andersson <hello@thejoltjoker.com>
2024-02-17 10:56:16 +0000

    Make logo and X stay on scroll when navbar open, scale up links on hover

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -13,96 +13,76 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="relative mx-auto flex w-full max-w-screen-xl items-center justify-between bg-pale-yellow px-sm py-sm">
-      <NavLink to="/" className="z-50">
-        <div className="max-w-logo-sm md:max-w-logo-md lg:max-w-logo-lg opacity-90">
-          <img
-            src={
-              isOpen
-                ? getImageUrl("logo_white.png")
-                : getImageUrl("logo_black.png")
-            }
-            alt="Vaca Caliente logo"
-            onClick={isOpen ? toggleNavigation : undefined}
-          />
-        </div>
-      </NavLink>
-      <input
-        type="checkbox"
-        name="menu"
-        id="menu"
-        className="hidden"
-        checked={isOpen}
-        onChange={toggleNavigation}
-      />
-      <label htmlFor="menu" className="z-50">
-        <MenuIcon isOpen={isOpen} />
-      </label>
+    <>
+      <nav className="h-navbar-sm md:h-navbar-md lg:h-navbar-lg relative mx-auto flex w-full max-w-screen-xl items-center justify-between bg-pale-yellow px-sm">
+        <NavLink to="/" className="z-50">
+          <div className="max-w-logo-sm md:max-w-logo-md lg:max-w-logo-lg opacity-90">
+            <img
+              src={
+                isOpen
+                  ? getImageUrl("logo_white.png")
+                  : getImageUrl("logo_black.png")
+              }
+              alt="Vaca Caliente logo"
+              onClick={isOpen ? toggleNavigation : undefined}
+            />
+          </div>
+        </NavLink>
+        <input
+          type="checkbox"
+          name="menu"
+          id="menu"
+          className="hidden"
+          checked={isOpen}
+          onChange={toggleNavigation}
+        />
+        <label htmlFor="menu" className="z-50">
+          <MenuIcon isOpen={isOpen} />
+        </label>
 
-      <div
-        className="fixed right-0 z-30 flex h-full w-full flex-col items-center justify-center bg-orange text-almost-white transition-all duration-500"
-        style={isOpen ? { top: 0 } : { top: "-100vh" }}
-      >
-        <ul
-          className="text-center font-heading text-4xl lg:text-6xl"
-          onClick={toggleNavigation}
+        <div
+          className="fixed right-0 z-30 flex h-full w-full flex-col items-center justify-center bg-orange text-almost-white transition-all duration-500"
+          style={isOpen ? { top: 0 } : { top: "-100vh" }}
         >
-          <li style={{ "--i": 0 } as React.CSSProperties}>
-            <NavLink
-              to="/"
-              className="transition-transform hover:scale-110 hover:text-pale-yellow"
-            >
-              Home
-            </NavLink>
-          </li>
-          <li style={{ "--i": 1 } as React.CSSProperties}>
-            <NavLink
-              to="/booking"
-              className="transition-transform hover:scale-110 hover:text-pale-yellow"
-            >
-              Book a Table
-            </NavLink>
-          </li>
-          <li style={{ "--i": 2 } as React.CSSProperties}>
-            <NavLink
-              to="/contact"
-              className="transition-transform hover:scale-110 hover:text-pale-yellow"
-            >
-              Contact
-            </NavLink>
-          </li>
-          <li style={{ "--i": 3 } as React.CSSProperties}>
-            <NavLink
-              to="/menu"
-              className="transition-transform hover:scale-110 hover:text-pale-yellow"
-            >
-              Menu
-            </NavLink>
-          </li>
-          <li style={{ "--i": 4 } as React.CSSProperties}>
-            <NavLink
-              to="/admin"
-              className="transition-transform hover:scale-110 hover:text-pale-yellow"
-            >
-              Admin
-            </NavLink>
-          </li>
-          <li className="social-icons flex justify-center gap-md pt-xl">
-            <Link to="#" style={{ "--i": 2 } as React.CSSProperties}>
-              <FaInstagram className="size-md cursor-pointer text-pale-yellow transition-all ease-in-out hover:scale-110 hover:text-almost-white-variant" />
-            </Link>
+          <ul
+            className="text-center font-heading text-4xl lg:text-6xl"
+            onClick={toggleNavigation}
+          >
+            <li style={{ "--i": 0 } as React.CSSProperties}>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li style={{ "--i": 1 } as React.CSSProperties}>
+              <NavLink to="/booking">Book a Table</NavLink>
+            </li>
+            <li style={{ "--i": 2 } as React.CSSProperties}>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+            <li style={{ "--i": 3 } as React.CSSProperties}>
+              <NavLink to="/menu">Menu</NavLink>
+            </li>
+            <li style={{ "--i": 4 } as React.CSSProperties}>
+              <NavLink to="/admin">Admin</NavLink>
+            </li>
+            <li className="social-icons flex justify-center gap-md pt-xl">
+              <Link to="#" style={{ "--i": 2 } as React.CSSProperties}>
+                <FaInstagram className="size-md cursor-pointer text-pale-yellow transition-all ease-in-out hover:scale-110 hover:text-almost-white-variant" />
+              </Link>
 
-            <Link to="#" style={{ "--i": 3 } as React.CSSProperties}>
-              <FaFacebook className="size-md cursor-pointer text-pale-yellow transition-all ease-in-out hover:scale-110 hover:text-almost-white-variant" />
-            </Link>
+              <Link to="#" style={{ "--i": 3 } as React.CSSProperties}>
+                <FaFacebook className="size-md cursor-pointer text-pale-yellow transition-all ease-in-out hover:scale-110 hover:text-almost-white-variant" />
+              </Link>
 
-            <Link to="#" style={{ "--i": 4 } as React.CSSProperties}>
-              <FaYelp className="size-md cursor-pointer text-pale-yellow transition-all ease-in-out hover:scale-110 hover:text-almost-white-variant" />
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+              <Link to="#" style={{ "--i": 4 } as React.CSSProperties}>
+                <FaYelp className="size-md cursor-pointer text-pale-yellow transition-all ease-in-out hover:scale-110 hover:text-almost-white-variant" />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      {isOpen && (
+        <div className="h-navbar-sm md:h-navbar-md lg:h-navbar-lg"></div>
+      )}
+    </>
   );
 };
 
