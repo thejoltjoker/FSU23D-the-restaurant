bd439337b451cec8907f91b28cdae329ea7e294e
Johannes Andersson <hello@thejoltjoker.com>
2024-02-19 16:56:41 +0000

    General update

--- src/components/BookingForm/BookingForm.tsx
+++ src/components/BookingForm/BookingForm.tsx
@@ -1,8 +1,42 @@
+import { format } from "date-fns";
+import { useState } from "react";
+import { Booking } from "../../models/Booking";
+import { Customer } from "../../models/Customer";
+import { getAvailableTimeSlots, restaurantId } from "../../services/restaurant";
 import "./BookingForm.css";
 const BookingForm = () => {
-  const getTimeSlots = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
+  const [timeSlots, setTimeSlots] = useState<string[]>();
+  const [booking, setBooking] = useState<Booking>(
+    new Booking(
+      restaurantId,
+      format(new Date(), "yyyy-MM-dd"),
+      "",
+      2,
+      new Customer("", "", "", ""),
+    ),
+  );
+  const getTimeSlots = async () => {
+    const slots = await getAvailableTimeSlots(
+      booking.restaurantId,
+      booking.date,
+      booking.numberOfGuests,
+    );
+
+    console.log(slots);
+    setTimeSlots(slots);
   };
+
+  const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setBooking({ ...booking, date: e.currentTarget.value });
+    getTimeSlots();
+  };
+  const handleGuestsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setBooking({ ...booking, numberOfGuests: Number(e.currentTarget.value) });
+    getTimeSlots();
+  };
+
   return (
     <>
       <h3 className="text-4xl">Make a reservation</h3>
@@ -10,33 +44,45 @@ const BookingForm = () => {
         <div className="flex gap-sm">
           <div className="shrink grow basis-1/2">
             <label htmlFor="date">Date:</label>
-            <input type="date" name="date" id="date" />
+            <input
+              type="date"
+              name="date"
+              id="date"
+              value={booking.date}
+              onChange={handleDateChange}
+            />
           </div>
           <div className="shrink grow basis-1/2">
             <label htmlFor="guests">Guests:</label>
-            <input type="number" name="guests" id="guests" className="w-full" />
+            <input
+              type="number"
+              name="guests"
+              id="guests"
+              className="w-full"
+              value={booking.numberOfGuests.toString()}
+              onChange={handleGuestsChange}
+            />
           </div>
         </div>
 
         <div className="time-slots">
           <p>Available time slots</p>
           <div className="time-slots flex gap-sm">
-            <input
-              type="radio"
-              id="time-slot-1"
-              name="time"
-              value="18:00"
-              className="hidden w-full"
-            />
-            <label htmlFor="time-slot-1">18:00</label>
-            <input
-              type="radio"
-              id="time-slot-2"
-              name="time"
-              value="21:00"
-              className="hidden"
-            />
-            <label htmlFor="time-slot-2">21:00</label>
+            {timeSlots
+              ? timeSlots.map((time, i) => (
+                  <>
+                    <input
+                      type="radio"
+                      key={`time-slot-${i}`}
+                      id={`time-slot-${i}`}
+                      name="time"
+                      value={time}
+                      className="hidden w-full"
+                    />
+                    <label htmlFor={`time-slot-${i}`}>{time}</label>
+                  </>
+                ))
+              : null}
           </div>
         </div>
       </form>
