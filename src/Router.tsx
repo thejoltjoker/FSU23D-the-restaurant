18ac5348fb2ed0799574dbd6e9395eccc6fbe486
Johannes Andersson <hello@thejoltjoker.com>
2024-02-19 13:06:44 +0000

    Update routes with new component names

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Adminpage from "./pages/AdminPage";
 import Bookingpage from "./pages/BookingPage";
 import Contactpage from "./pages/ContactPage";
-import Errorpage from "./pages/ErrorPage";
+import ErrorPage from "./pages/ErrorPage";
 import Homepage from "./pages/HomePage";
 import Layout from "./pages/Layout";
 import Menupage from "./pages/MenuPage";
@@ -13,33 +13,28 @@ export const router = createBrowserRouter(
     {
       path: "/",
       element: <Layout />,
-      errorElement: <Errorpage />,
+      errorElement: <ErrorPage />,
       children: [
         {
           path: "/",
           index: true,
           element: <Homepage />,
-          errorElement: <Errorpage />,
         },
         {
           path: "/contact",
           element: <Contactpage />,
-          errorElement: <Errorpage />,
         },
         {
           path: "/booking",
           element: <Bookingpage />,
-          errorElement: <Errorpage />,
         },
         {
           path: "/menu",
           element: <Menupage />,
-          errorElement: <Errorpage />,
         },
         {
           path: "/admin",
           element: <Adminpage />,
-          errorElement: <Errorpage />,
         },
         {
           path: "/styles",
