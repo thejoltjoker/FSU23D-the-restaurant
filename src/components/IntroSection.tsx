16ac0c2116d91bd4c108ec4de94a894c73817b58
timothy listam <timothylistam@gmail.com>
2024-02-21 04:17:20 +0100

    Create a new IntroSection component from Menupage

---
+++ src/components/IntroSection.tsx
@@ -0,0 +1,24 @@
+import BookButton from "./BookButton";
+import OrderButton from "./OrderButton";
+
+const IntroSection = () => {
+  return (
+    <div className="h-[1000px] p-5 md:w-2/3">
+      <h1 className="md:heading-md mb-5 mt-5 text-heading-sm lg:text-heading-lg">
+        Menu
+      </h1>
+      <p className="md:paragraph-md mb-5 text-paragraph-sm lg:text-paragraph-lg">
+        Discover a culinary fiesta at Vaca Caliente! Our menu is a vibrant
+        tapestry of authentic Mexican flavors, expertly crafted to tantalize
+        your taste buds. From sizzling tacos to savory burritos and indulgent
+        desserts, every dish is a celebration of bold and delicious experiences.
+      </p>
+      <div className="flex gap-5">
+        <OrderButton />
+        <BookButton />
+      </div>
+    </div>
+  );
+};
+
+export default IntroSection;
